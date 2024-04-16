9fad879a3f34fa76878447bbf93d4ef54f45d799
evanfrnando25 <evanfrnando@gmail.com>
2024-04-16 12:45:39 +0700

    fix align

--- components/common/Header.vue
+++ components/common/Header.vue
@@ -12,24 +12,33 @@
 </template>
 
 <style lang="scss" scoped>
-.header {
-    &__wrapper {
-        display: flex;
-        justify-content: flex-end; /* Menggeser konten ke kanan */
-    }
+    .header {
+        &__wrapper {
+            display: flex;
+            justify-content: flex-end; /* Menggeser konten ke kanan */
+        }
 
-    &__content {
-        /* Tidak ada perubahan pada bagian ini */
-        display: flex;
-    }
+        &__content {
+            /* Tidak ada perubahan pada bagian ini */
+            display: flex;
+            align-items: center;
+            align-content: center;
+        }
 
-    &__content__user {
-        width: 90px;
-        border: 1px solid #016DD3;
-        border-radius: 10px;
-        padding: 10px 19px;
-        display: flex;
-        justify-content: space-between;
+        &__content__user {
+            width: 90px;
+            border: 1px solid #016dd3;
+            border-radius: 10px;
+            padding: 10px 19px;
+            display: flex;
+            gap: 10%;
+            justify-content: space-between;
+
+            span {
+                align-items: center;
+                align-content: center;
+                align-self: center;
+            }
+        }
     }
-}
 </style>
--- components/common/Table.vue
+++ components/common/Table.vue
@@ -1,140 +1,142 @@
 <template>
-  <div class="table">
-    <q-table flat bordered :rows="data" :columns="columns" row-key="pageIndex">
-      <template v-slot:top>
-        <span class="table-search">
-          <input type="text" />
-        </span>
-        <q-space />
-        <span class="table-export">
-          <button>Export Excel</button>
-        </span>
-      </template>
-      <template v-slot:header="props">
-        <q-tr :props="props">
-          <q-th>No.</q-th>
-          <q-th v-for="col in props.cols" :key="col.name" :props="props">
-            {{ col.label }}
-          </q-th>
-          <q-th v-if="action">Action</q-th>
-        </q-tr>
-      </template>
-      <template v-slot:body="props">
-        <q-tr :props="props">
-          <q-td auto-width>
-            {{ props.pageIndex + 1 }}
-          </q-td>
-          <q-td v-for="col in props.cols" :key="col.name" :props="props">
-            {{ col.value }}
-          </q-td>
-          <q-td class="text-center" v-if="action">
-            <div class="action-section">
-              <button
-                v-for="items in action"
-                :key="items.id"
-                :class="'button-' + items.label.toLowerCase()"
-                @click="
-                  items.params === true
-                    ? $emit(
-                        `handle${items.label}`,
-                        props.row[items.routeParams]
-                      )
-                    : $emit(`handle${items.label}`)
-                "
-              >
-                {{ items.label }}
-              </button>
-            </div>
-          </q-td>
-        </q-tr>
-      </template>
-    </q-table>
-  </div>
+    <div class="table">
+        <q-table flat bordered :rows="data" :columns="columns" row-key="pageIndex">
+            <template v-slot:top>
+                <span class="table-search">
+                    <input type="text" />
+                </span>
+                <q-space />
+                <span class="table-export">
+                    <button>Export Excel</button>
+                </span>
+            </template>
+            <template v-slot:header="props">
+                <q-tr :props="props">
+                    <q-th>No.</q-th>
+                    <q-th v-for="col in props.cols" :key="col.name" :props="props">
+                        {{ col.label }}
+                    </q-th>
+                    <q-th v-if="action">Action</q-th>
+                </q-tr>
+            </template>
+            <template v-slot:body="props">
+                <q-tr :props="props">
+                    <q-td auto-width>
+                        {{ props.pageIndex + 1 }}
+                    </q-td>
+                    <q-td v-for="col in props.cols" :key="col.name" :props="props">
+                        {{ col.value }}
+                    </q-td>
+                    <q-td class="text-center" v-if="action">
+                        <div class="action-section">
+                            <button
+                                v-for="items in action"
+                                :key="items.id"
+                                :class="'button-' + items.label.toLowerCase()"
+                                @click="
+                                    items.params === true
+                                        ? $emit(
+                                              `handle${items.label}`,
+                                              props.row[items.routeParams],
+                                          )
+                                        : $emit(`handle${items.label}`)
+                                "
+                            >
+                                {{ items.label }}
+                            </button>
+                        </div>
+                    </q-td>
+                </q-tr>
+            </template>
+        </q-table>
+    </div>
 </template>
 
 <script lang="ts">
-
-export default defineNuxtComponent({
-  props: {
-    columns: {
-      required: false,
-      type: Array || null,
-    },
-    data: {
-      required: false,
-      type: Array || null,
-    },
-    action: {
-      required: false,
-      type: Array || null,
-    },
-  },
-  setup(props: any, { emit }: any) {
-    return {};
-  },
-});
-
+    export default defineNuxtComponent({
+        props: {
+            columns: {
+                required: false,
+                type: Array || null,
+            },
+            data: {
+                required: false,
+                type: Array || null,
+            },
+            action: {
+                required: false,
+                type: Array || null,
+            },
+        },
+        setup(props: any, { emit }: any) {
+            return {}
+        },
+    })
 </script>
 
 <style lang="scss" scoped>
+    .table-search {
+        input {
+            border-radius: 10px;
+            border: 1px solid #ddd;
+            height: 47px;
+            padding: 2% 4%;
+            width: 100%;
+            outline: none;
 
-.table-search {
-  input {
-    border-radius: 10px;
-    border: 1px solid #ddd;
-    height: 47px;
-    padding: 2% 4%;
-    width: 100%;
-    outline: none;
-
-    @media (min-width: 768px) {
-      padding: 5% 3% 5% 5%;
+            @media (min-width: 768px) {
+                padding: 5% 3% 5% 5%;
+            }
+        }
     }
-  }
-}
 
-.table-export {
-  button {
-    border-radius: 5px;
-    background: $primary-100;
-    padding: 10% 10px;
-    color: $basic;
-    border: none;
-    cursor: pointer;
-  }
-}
+    .table-export {
+        button {
+            border-radius: 5px;
+            background: $primary-100;
+            margin-top: 15px;
+            padding: 10% 10px;
+            color: $basic;
+            border: none;
+            cursor: pointer;
 
-.action-section {
-  justify-content: center;
-  display: flex;
-  gap: 5%;
-  position: relative;
+            @media (min-width: 768px) {
+                margin-top: 0px;
+            }
+        }
+    }
 
-  .button-detail {
-    background: $primary-100;
-    color: $basic;
-    padding: 15% 20%;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-  }
+    .action-section {
+        justify-content: center;
+        display: flex;
+        gap: 5%;
+        position: relative;
 
-  .button-submerchant {
-    background: $primary-100;
-    color: $basic;
-    padding: 8% 10%;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-  }
+        .button-detail {
+            background: $primary-100;
+            color: $basic;
+            padding: 15% 20%;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
 
-  .button-process {
-    background: $primary-100;
-    color: $basic;
-    padding: 8% 10%;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-  }
-}
+        .button-submerchant {
+            background: $primary-100;
+            color: $basic;
+            padding: 8% 10%;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        .button-process {
+            background: $primary-100;
+            color: $basic;
+            padding: 8% 10%;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+    }
 </style>
